1ad45acdbdc0db3738f5ff8970c3e7b6d2330f8f
super-truite <super-truite@hotmail.fr>
2023-01-08 16:55:46 +0100

    handling streamlosterror

--- il2_queuing_rc/remote_console_client.py
+++ il2_queuing_rc/remote_console_client.py
@@ -20,6 +20,26 @@ class RemoteConsoleRpcClient(object):
 
         self.response = None
         self.corr_id = None
+    
+    def reset(self):
+        '''
+        initialize again the connection (in case of Streamlost error for instance)
+        '''
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost', heartbeat=10)) 
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='')
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+        self.response = None
+        self.corr_id = None
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
@@ -29,16 +49,22 @@ class RemoteConsoleRpcClient(object):
         print(" [x] Requesting %r" % msg)
         self.response = None
         self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_queue',
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body=msg)
-        self.connection.process_data_events(time_limit=None)
-        print(" [.] Got %r" % self.response)
+        try:
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='rpc_queue',
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue,
+                    correlation_id=self.corr_id,
+                ),
+                body=msg)
+            self.connection.process_data_events(time_limit=None)
+            print(" [.] Got %r" % self.response)
+        except pika.exceptions.StreamLostError:
+            print('connection closed... and restarted')
+            self.reset()
+            self.call(msg)
+
         return self.response
 
 if __name__ == '__main__': 
